27771371c73429f97b7aeb6e17ab4384acfe2c4e
vinitdawane <45765884+vinitdawane@users.noreply.github.com>
2026-07-19 15:24:25 +0530

    Create Python_AI.py
    Python Code for Stramlit Web App using Gemini

---
+++ Python_AI.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from google import genai
+
+st.markdown(
+    """
+    <h1 style= 'text-align: center;'> My Personal AI Assistant </h1>
+    <p style= 'text-align: center; font-size:28px;'>
+        Qunch your curiosity
+    </p>
+    """,
+    unsafe_allow_html = True,
+)
+
+
+
+robo = genai.Client(api_key = "MY_API_KEY")
+
+mychat = robo.chats.create(model="gemini-3.1-flash-lite")
+
+response_placeholder = st.empty()
+
+question = st.text_input("Ask me something : ")
+
+col1, col2, col3 = st.columns([4,1,4])
+
+with col2 :
+    send = st.button("Send")
+    
+if send:
+    response = mychat.send_message(question)
+    response_placeholder.write(response.text)
